9be2bee883593b75a3764ccd861aaadd56e823a1
june <luo.jnny@gmail.com>
2022-02-03 20:51:48 -0800

    comments, puts and print, variable, data type

---
+++ ruby-notes.rb
@@ -0,0 +1,27 @@
+# why ruby?
+
+# single line comment using hashtag
+
+=begin
+    use =being to start multi line comment 
+    and =end to end it
+=end
+
+# display to screen. print vs puts
+# print command - prints everything in one line with no space
+print "hello"
+print "world"    # those 2 print will display: helloworld 
+
+# puts command - adds a line break
+puts "hello"    # display: hello
+puts "world"    # display on another line: world
+
+# declare variable, = to assign it a value
+name_of_variable = "value"
+
+# data type
+my_num = 0   # numeric, any numbers 
+my_boolean = true   # not capitalize
+my_string = "hello world"
+
+# has 6 arithmetic operator: +, -, *, /, ** (exponent), % (modulo, remainder of division)
